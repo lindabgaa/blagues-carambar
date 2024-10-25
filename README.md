39d833d7bf1959e173d3c1b34db666dfb1e414dc
Linda BOUGAA <lindabgaa@Host-003.lan>
2024-10-26 01:22:03 +0200

    setup backend : database + api

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ![Project Status](https://img.shields.io/badge/Project%20Status-In%20Progress-orange)
 
+Ce projet est une application web permettant aux utilisateurs d’obtenir une blague Carambar en un simple clique. L’application repose sur une **API REST versionnée**, développée avec **Node.js**, **Express**, et **Sequelize**, et utilise une base de données **SQLite** pour stocker les blagues. L’API propose des endpoints pour ajouter des blagues, en obtenir une aléatoire, ou consulter la liste complète. La documentation de l’API est gérée avec **Swagger**, facilitant son utilisation et son intégration. Le projet est déployé sur **Render.com** pour l'API et sur **GitHub Pages** pour le frontend.
+
 ## Technologies
 
 - **Frontend**: React, Vite, Axios, CSS
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const swagger = require("./config/swagger");
 
+const sequelize = require("./config/database.js");
+const jokeRoutes = require("./routes/jokeRoutes.js");
+
+const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Configuration CORS
@@ -11,11 +15,58 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/api", jokeRoutes);
+swagger(app);
 
-app.get("/", (req, res) => {
-  res.send("Hello from server!");
+app.get("/api", (req, res) => {
+  const response = {
+    message: "Bienvenue sur l'API de blagues Carambar!",
+    description:
+      "Cette API permet de gérer une collection de blagues Carambar.",
+    endpoints: {
+      randomJoke: {
+        method: "GET",
+        path: "/api/blagues/random",
+        description: "Récupérer une blague aléatoire.",
+      },
+      allJokes: {
+        method: "GET",
+        path: "/api/blagues",
+        description: "Lister toutes les blagues disponibles.",
+      },
+      getJokeById: {
+        method: "GET",
+        path: "/api/blagues/:id",
+        description: "Récupérer une blague précise en utilisant son ID.",
+      },
+      addJoke: {
+        method: "POST",
+        path: "/api/blagues",
+        description: "Ajouter une nouvelle blague.",
+      },
+    },
+    documentation: {
+      url: "/api-docs",
+      description: "Consultez la documentation de l'API ici.",
+    },
+    version: "1.0.0",
+  };
+
+  res.json(response);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+sequelize
+  .sync()
+  .then(() => {
+    console.log("La base de données est synchronisée ! ");
+    app.listen(PORT, () => {
+      console.log(`Le serveur est lancé sur le port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(
+      "Erreur lors de la synchronisation de la base de donnée :",
+      err
+    );
+  });
